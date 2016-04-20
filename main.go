3d43a1911679939747a37dfa58bb3c6a6b5f9541
JoeSelvik <joe@thenewtricks.com>
2016-04-20 09:32:36 -0600

    Basics done, get user map for access token, groupid and look at the herpderp feed

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	fb "github.com/huandu/facebook"
+)
+
+// TODO: modularize, ie ask for what kind of acess and for which user
+func GetAccessToken() string {
+	var accessToken = "CAACEdEose0cBANZBMXBYPhpegCUyx3B54uCJNFnOCyhnOAhqNteXwEwCTWZAYMW6HZCGLg4sHp83gVap1uiKKbz6Wjvh8iJf57LoZC1Tol3ZBZA3CKAWgERwxwANF4ghpObWnSFZAj8SszlRtUOJ6hfj4FZAIpYP7if4tBqWz7o2ByZCXquHGqIgWrP2BU1WcxpDB9iAtALp6Deizgb704i96"
+	return accessToken
+}
+
+// GetUserID will return the user's id associated with the access token provided for the app.
+//
+// May modify this in the future to just return the user map.
+func GetUserID() string {
+	var myAccessToken = GetAccessToken()
+
+	res, err := fb.Get("/me", fb.Params{
+		"access_token": myAccessToken,
+	})
+	if err != nil {
+		fmt.Println("Error when accessing /me: ", err)
+	}
+
+	fmt.Println("User associated with access token: ", res)
+
+	// TODO: is type assertion a bad idea here? Just handle the error from .Get?
+	return res["id"].(string)
+}
+
+// TODO: modularize, ie ask for which group
+func GetGroupID() string {
+	var groupID = "208678979226870"
+	return groupID
+}
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+	var myAccessToken = GetAccessToken()
+	var _ = GetUserID()
+	var herpDerpGroupID = GetGroupID()
+
+	// Start parsing through the feed!
+	// Params: can specify which fields you want back
+	res, err := fb.Get(fmt.Sprintf("/%s/feed", herpDerpGroupID), fb.Params{
+		// "fields":       "feed",
+		"access_token": myAccessToken,
+	})
+	if err != nil {
+		fmt.Println("Error when getting group feed: ", err)
+	}
+
+	// grab 'data' for a map of posts
+	fmt.Println("Feed length:", len(res))
+	for k, _ := range res {
+		fmt.Println(k)
+	}
+
+	// Extract post data for users
+	// example API call on post gotten from feed
+	// 208678979226870_1036221373139289?fields=attachments,comments,likes,from,description,created_time,name,picture,status_type,type,caption
 }
