b2b4202974821f9bc462f63f79ac0666180fe1a5
JoeSelvik <joe@thenewtricks.com>
2016-12-18 14:27:34 -0700

    added function to get posts from herp

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	fb "github.com/huandu/facebook"
+	"os"
 )
 
 // GetAccessToken returns the access token needed to make authenticated requests
 //
 // Generated at https://developers.facebook.com/tools/explorer/
 func GetAccessToken() string {
-	var accessToken = "EAACEdEose0cBAMcn6ZCpQTQZBJ80Q1fhZBKC5ivKLSJ1ZAvnLUiax5SYt1DZBG8E7ZC8cjtHb5adrj1Y8apxtSHXWbZAZAkZCzZBNl8tZBNyYVjCnW8oZCEPLOMBunu4MJGL4agh7mop57rrDo55JAX1KMioE8S34pVuDcFVZAMKpwEry9AZDZD"
+	var accessToken = "EAACEdEose0cBAGySW5F9i2Ja2NMQHEZB8qxCL4JOVHlAhxyTSISZBhJPZCGZApvm4p5zncdWMyMtzAeayG6bIlZCqgrJpPErmNXWPsKAShOW1LFVdEZCMpbM7kaUvRAFZB40PDSQUeJjEGxZBWEvAIEtustHmgPPd1VRgZBS31RuUxwZDZD"
 	return accessToken
 }
 
@@ -24,6 +25,7 @@ func GetUserID() string {
 	})
 	if err != nil {
 		fmt.Println("Error when accessing /me: ", err)
+		os.Exit(3)
 	}
 
 	fmt.Println("User associated with access token: ", res)
@@ -47,32 +49,22 @@ type Contender struct {
 	TotalLikesGiven    int
 }
 
-func main() {
-	var myAccessToken = GetAccessToken()
-	var herpDerpGroupID = GetGroupID()
-
-	// "your-app-id", "your-app-secret", from 'development' app I made
-	var globalApp = fb.New("756979584457445", "023c1d8f5e901c2111d7d136f5165b2a")
-	session := globalApp.Session(myAccessToken)
-	err := session.Validate()
-	if err != nil {
-		fmt.Println("Error validating session:", err)
-	}
-
-	// Get list of all members
+// Returns a slice of Contenders for a given *Session
+func CreateContenders(session *fb.Session) []Contender {
 	// response is a map[string]interface{}
-	response, err := fb.Get(fmt.Sprintf("/%s/members", herpDerpGroupID), fb.Params{
-		// "fields":       "feed",
-		"access_token": myAccessToken,
+	response, err := fb.Get(fmt.Sprintf("/%s/members", GetGroupID()), fb.Params{
+		"access_token": GetAccessToken(),
 	})
 	if err != nil {
 		fmt.Println("Error when getting group members:", err)
+		os.Exit(3)
 	}
 
 	// Create the paging object for /members response
 	paging, err := response.Paging(session)
 	if err != nil {
-		fmt.Println("Error when generating the responses Paging object:", err)
+		fmt.Println("Error when generating the members responses Paging object:", err)
+		os.Exit(3)
 	}
 
 	var contenders []Contender
@@ -86,19 +78,84 @@ func main() {
 		for i := 0; i < len(results); i++ {
 			results[i].Decode(&c)
 			contenders = append(contenders, c)
-			fmt.Println("Contender added", c)
 		}
 
 		noMore, err := paging.Next()
 		if err != nil {
 			fmt.Println("Error when accessing responses Next in loop:", err)
+			os.Exit(3)
 		}
 		if noMore {
 			break
 		}
 	}
 
+	return contenders
+}
+
+func populateTotalPosts(contenders []Contender, session *fb.Session) {
+	response, err := fb.Get(fmt.Sprintf("/%s/feed", GetGroupID()), fb.Params{
+		"access_token": GetAccessToken(),
+		"feilds":       "from",
+	})
+	if err != nil {
+		fmt.Println("Error when getting feed:", err)
+		os.Exit(3)
+	}
+
+	paging, err := response.Paging(session)
+	if err != nil {
+		fmt.Println("Error when generating the feed responses Paging object:", err)
+		os.Exit(3)
+	}
+
+	var posts []interface{}
+	count := 0
+
+	for {
+		results := paging.Data()
+
+		for i := 0; i < len(results); i++ {
+			posts = append(posts, results[i])
+		}
+		count++
+		fmt.Println("finished lap:", count)
+
+		if count > 10 {
+			fmt.Println("found first 200 posts")
+			break
+		}
+
+		noMore, err := paging.Next()
+		if err != nil {
+			fmt.Println("Error when accessing responses Next in loop:", err)
+			os.Exit(3)
+		}
+		if noMore {
+			break
+		}
+	}
+	fmt.Println("number posts:", len(posts))
+	fmt.Println("First post:", posts[1])
+}
+
+func main() {
+	var myAccessToken = GetAccessToken()
+	// var herpDerpGroupID = GetGroupID()
+
+	// "your-app-id", "your-app-secret", from 'development' app I made
+	var globalApp = fb.New("756979584457445", "023c1d8f5e901c2111d7d136f5165b2a")
+	session := globalApp.Session(myAccessToken)
+	err := session.Validate()
+	if err != nil {
+		fmt.Println("Error validating session:", err)
+		os.Exit(3)
+	}
+
+	contenders := CreateContenders(session)
 	fmt.Println("number of members:", len(contenders))
+
+	populateTotalPosts(contenders, session)
 
 	// Extract post data for users
 	// example API call on post gotten from feed
